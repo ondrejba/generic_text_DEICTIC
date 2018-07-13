35eaa64a0134dd811f82afcd831bbac5a278fed7
atenpas <andreas.tenpas@gmail.com>
2018-07-13 17:22:38 -0400

    changed models

--- models.py
+++ models.py
@@ -78,6 +78,82 @@ def _cnn_to_mlp(convs, hiddens, dueling, inpt, num_actions, scope, reuse=False):
         else:
             return action_scores
 
+def _cnn_to_mlp_state_direct(convs, hiddens, dueling, action_input, state_input, num_states, num_actions, scope,
+                             reuse=False):
+
+    state_input = tf.one_hot(state_input, num_states, dtype=tf.float32)
+
+    with tf.variable_scope(scope, reuse=reuse):
+        out = action_input
+        with tf.variable_scope("convnet"):
+            for num_outputs, kernel_size, stride in convs:
+                out = layers.convolution2d(out,
+                                           num_outputs=num_outputs,
+                                           kernel_size=kernel_size,
+                                           stride=stride,
+                                           activation_fn=tf.nn.relu)
+
+        out = layers.flatten(out)
+        out = tf.concat((out, state_input), axis=1)
+
+        with tf.variable_scope("action_value"):
+            action_out = out
+            for hidden in hiddens:
+                action_out = layers.fully_connected(action_out, num_outputs=hidden, activation_fn=tf.nn.relu)
+            action_scores = layers.fully_connected(action_out, num_outputs=num_actions, activation_fn=None)
+
+        if dueling:
+            with tf.variable_scope("state_value"):
+                state_out = out
+                for hidden in hiddens:
+                    state_out = layers.fully_connected(state_out, num_outputs=hidden, activation_fn=tf.nn.relu)
+                state_score = layers.fully_connected(state_out, num_outputs=1, activation_fn=None)
+            action_scores_mean = tf.reduce_mean(action_scores, 1)
+            action_scores_centered = action_scores - tf.expand_dims(action_scores_mean, 1)
+            return state_score + action_scores_centered
+        else:
+            return action_scores
+
+
+def _cnn_to_mlp_state_embed(convs, hiddens, dueling, action_input, state_input, num_states, state_embedding_size,
+                            num_actions, scope, reuse=False):
+
+    with tf.variable_scope(scope, reuse=reuse):
+        out = action_input
+        with tf.variable_scope("convnet"):
+            for num_outputs, kernel_size, stride in convs:
+                out = layers.convolution2d(out,
+                                           num_outputs=num_outputs,
+                                           kernel_size=kernel_size,
+                                           stride=stride,
+                                           activation_fn=tf.nn.relu)
+
+        with tf.variable_scope("embedding"):
+
+            embedded_state_t = embed(state_input, num_states, state_embedding_size)
+            embedded_state_t = tf.nn.relu(embedded_state_t)
+
+        out = layers.flatten(out)
+        out = tf.concat((out, embedded_state_t), axis=1)
+
+        with tf.variable_scope("action_value"):
+            action_out = out
+            for hidden in hiddens:
+                action_out = layers.fully_connected(action_out, num_outputs=hidden, activation_fn=tf.nn.relu)
+            action_scores = layers.fully_connected(action_out, num_outputs=num_actions, activation_fn=None)
+
+        if dueling:
+            with tf.variable_scope("state_value"):
+                state_out = out
+                for hidden in hiddens:
+                    state_out = layers.fully_connected(state_out, num_outputs=hidden, activation_fn=tf.nn.relu)
+                state_score = layers.fully_connected(state_out, num_outputs=1, activation_fn=None)
+            action_scores_mean = tf.reduce_mean(action_scores, 1)
+            action_scores_centered = action_scores - tf.expand_dims(action_scores_mean, 1)
+            return state_score + action_scores_centered
+        else:
+            return action_scores
+
 def _cnn_to_mlp_symbolic(convs, hiddens, deictic_input, abstract_input, num_actions, num_abstract_actions,
                          abstract_embedding_size, scope, reuse=False):
 
@@ -136,7 +212,6 @@ def _cnn_to_mlp_symbolic_multiplex(convs, hiddens, deictic_input, abstract_input
         out += multiplex_t
         out = layers.flatten(out)
 
-
         with tf.variable_scope("action_value"):
 
             action_out = out
@@ -167,6 +242,14 @@ def cnn_to_mlp(convs, hiddens, dueling=False):
     """
 
     return lambda *args, **kwargs: _cnn_to_mlp(convs, hiddens, dueling, *args, **kwargs)
+
+def cnn_to_mlp_state_direct(convs, hiddens, dueling=False):
+
+    return lambda *args, **kwargs: _cnn_to_mlp_state_direct(convs, hiddens, dueling, *args, **kwargs)
+
+def cnn_to_mlp_state_embed(convs, hiddens, dueling=False):
+
+    return lambda *args, **kwargs: _cnn_to_mlp_state_embed(convs, hiddens, dueling, *args, **kwargs)
 
 def cnn_to_mlp_symbolic(convs, hiddens):
 
